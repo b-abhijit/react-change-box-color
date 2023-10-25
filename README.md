0b9cd61c242d33d7325389545be8fb1345dd4c74
abhijit <abhijit.guist@gmail.com>
2023-10-25 22:27:39 +0530

    change box color based on user input
    Netlify demo link added

---
+++ README.md
@@ -0,0 +1,5 @@
+# React + Vite
+
+Change box color based on input color given by user
+
+Demo link - https://react-change-box-color.netlify.app/
---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import './style.css'
+
+function App() {
+  // eslint-disable-next-line no-unused-vars
+  const [color, setColor] = useState("")
+
+  function changeColor(newColor) {
+    console.log(newColor);
+    setColor(newColor);
+  }
+
+  return (
+    <>
+      {console.log("render")}
+      <div className='container'>
+        <div className="box" style={{ background: `${color}`, border: `1px solid ${color}` }}></div>
+        <input onChange={e => changeColor(e.target.value)} type="text" id="color"/>
+      </div>
+    </>
+  )
+}
+
+export default App
